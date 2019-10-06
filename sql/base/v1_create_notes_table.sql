45f7a77430b5ed952e3b4cacc9ba012af14f615b
marco <marco.navarro@hulilabs.com>
2019-10-05 18:02:09 -0600

    Better column name

--- sql/base/v1_create_notes_table.sql
+++ sql/base/v1_create_notes_table.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE note
 (
-    id int PRIMARY KEY AUTO_INCREMENT,
+    id_note int PRIMARY KEY AUTO_INCREMENT,
     content text,
     status enum('active', 'archived', 'deleted') default 'active',
     created_on datetime default now(),
